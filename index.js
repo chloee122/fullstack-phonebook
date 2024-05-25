d04d91224f03c5a4cda0f2192e7a3bb512aeee26
Chloe <nguyeney02@gmail.com>
2024-05-25 13:47:24 +0300

    WIP

--- index.js
+++ index.js
@@ -21,8 +21,6 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
-  } else {
-    return response.status(500).send({ error: "Error occurs" });
   }
 
   next(error);
@@ -58,7 +56,7 @@ app.delete("/api/persons/:id", (request, response, next) => {
   const id = request.params.id;
 
   Person.findByIdAndDelete(id)
-    .then((result) => response.status(204).end())
+    .then((result) => response.status(404).end())
     .catch((error) => next(error));
 });
 
@@ -70,14 +68,6 @@ app.post("/api/persons", (request, response, next) => {
       error: "content missing",
     });
   }
-
-  // const personExists = entries.find((person) => person.name === name);
-
-  // if (personExists) {
-  //   return response.status(400).json({
-  //     error: "name already exists",
-  //   });
-  // }
 
   const person = new Person({
     name,
@@ -95,12 +85,27 @@ app.post("/api/persons", (request, response, next) => {
 app.put("/api/persons/:id", (request, response, next) => {
   const updatedPerson = request.body;
   const id = request.params.id;
-
+  // Person.findById(id).then((person) => {
+  //   if (!person) {
+  //     response.status(404).json({
+  //       error: `${updatedPerson.name} was already removed from server`,
+  //     });
+  //   } else {
+  //     Person.findByIdAndUpdate(id, updatedPerson, { new: true })
+  //       .then((returnedPerson) => response.json(returnedPerson))
+  //       .catch((err) => next(err));
+  //   }
+  // });
   Person.findByIdAndUpdate(id, updatedPerson, { new: true })
     .then((returnedPerson) => response.json(returnedPerson))
     .catch((err) => next(err));
 });
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
 app.use(errorHandler);
 
 const PORT = process.env.PORT;
